1a288cfa3a61ad43c333f0e05fd4c7f631c4365a
Ahmed Zedan <zedan@gmail.com>
2023-10-19 03:16:29 +0300

    responsive design

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -18,29 +18,21 @@ function Categories(){
         initialSlide: 0,
         responsive: [
           {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
+            breakpoint: 1480,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
+              slidesToScroll: 1,
+              infinite: true,
             }
           },
           {
-            breakpoint: 480,
+            breakpoint: 1000,
             settings: {
               slidesToShow: 1,
-              slidesToScroll: 1
+              slidesToScroll: 1,
+              infinite: true,
             }
-          }
+          },
         ]
     };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,32 +13,36 @@ function Header(){
     return (
         <div className={styles.main}>
             <Logo />
-            <div className={styles.container2}>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="menu">Menu</NavLink>
-                <NavLink to="offers">Offers</NavLink>
-                <NavLink to="book">Book Table</NavLink>
-                <NavLink to="contact">Contact Us</NavLink>
-            </div>
-            <div className={styles.container1}>
-                <div className={styles.container2}>
-                    <div className={styles.cart} onClick={()=>setOpenCart((e)=>!e)}>
-                        <img src="img/cart.svg" alt="cart"/>
-                        <span>{numberOfItems}</span>
-                    </div>
-                    <div className={styles.search}>
-                        <input type="text" placeholder="Search"/>
-                        <img src="img/search.svg" alt="search"/>
-                    </div>
+            <input type="checkbox" id="headerList" className={styles.headerList}/>
+            <label htmlFor="headerList" className={styles.iconList}> &#9776; </label>
+            <div className={styles.responsive}>
+                <div className={`${styles.container2} ${styles.nav}`}>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="menu">Menu</NavLink>
+                    <NavLink to="offers">Offers</NavLink>
+                    <NavLink to="book">Book Table</NavLink>
+                    <NavLink to="contact">Contact Us</NavLink>
                 </div>
-                {
-                    !login ? 
-                    <div className={`${styles.container2} ${styles.btns}`}>
-                        <Button styleType="btn1" onClick={()=>navigate("/login")}>Login</Button>
-                        <Button styleType="btn2" onClick={()=>navigate("/signup")}>Sign up</Button>
-                    </div> :
-                    <Profile/>
-                }
+                <div className={styles.container1}>
+                    <div className={styles.container2}>
+                        <div className={styles.cart} onClick={()=>setOpenCart((e)=>!e)}>
+                            <img src="img/cart.svg" alt="cart"/>
+                            <span>{numberOfItems}</span>
+                        </div>
+                        <div className={styles.search}>
+                            <input type="text" placeholder="Search"/>
+                            <img src="img/search.svg" alt="search"/>
+                        </div>
+                    </div>
+                    {
+                        !login ? 
+                        <div className={`${styles.container2} ${styles.btns}`}>
+                            <Button styleType="btn1" onClick={()=>navigate("/login")}>Login</Button>
+                            <Button styleType="btn2" onClick={()=>navigate("/signup")}>Sign up</Button>
+                        </div> :
+                        <Profile/>
+                    }
+                </div>
             </div>
         </div>
     );
